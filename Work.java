d1f7ab3f4b61aef58521dc6414efd6d7da40da7f
Nikita <litnik99@mail.ru>
2022-02-23 23:53:46 +0400

    Второй коммит: Work.java Добавлен счетчик подсчета числа продолжений работы

--- Work.java
+++ Work.java
@@ -1,9 +1,12 @@
+package com.company.Workraft;
+
 //import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Work{
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
+        int counter = 0;
         while(true){
             System.out.println("1. Continue working");
             System.out.println("2. Stop working");
@@ -13,6 +16,8 @@ public class Work{
             }
             else if (step != 1)
                 System.out.println("WRONG WAY");
+            else counter += 1;
         }
+        System.out.println("The worker is working " + counter + " times");
     }
 }
